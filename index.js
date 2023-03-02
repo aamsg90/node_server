514e2cdb491389499fd1e0e3b2fe6dd313d0965d
Ahmed <aamsg90@gmail.com>
2023-03-01 22:39:11 -0500

    final result

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-// import your http
-const http = require("http")
+const http = require('http');
+const url = require('url');
 
-// creat server with HTTP
-const server = http.createServer((req, res)=> {
-    console.log("server is created")
-})
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
 
-// initiate port
-const PORT = 4000
+  if (path === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Welcome to my server!</h1>');
+    res.write('<img src="https://picsum.photos/200/300">');
+    res.end();
+  } else if (path === '/list') {
+    fetch('https://swapi.dev/api/people')
+      .then(response => response.json())
+      .then(data => {
+        const people = data.results;
+        // res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<table>');
+        res.write('<tr><th>Name</th><th>Height</th><th>Birth Year</th><th>Gender</th><th>url</th></tr>');
+        for (let person of people) {
+          res.write(`<tr><td>${person.name}</td><td>${person.height}</td><td>${person.birth_year}</td><td>${person.gender}</td><td><a href="${person.url}">url</a></td></tr>`);
+        }
+        res.write('</table>');
+        res.end();
+      })
+      .catch(error => {
+        console.log(error);
+        res.writeHead(500, {'Content-Type': 'text/html'});
+        res.write('<h1>Internal Server Error</h1>');
+        res.end();
+      });
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.write('<h1>404 Page Not Found</h1>');
+    res.end();
+  }
+});
 
-// lisiten to server
-server.listen(PORT, ()=> console.log(`server is runing on port ${PORT}`))
-
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
